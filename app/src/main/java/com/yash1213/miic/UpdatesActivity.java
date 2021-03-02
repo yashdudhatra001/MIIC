49f69e2ddd3177814d77a36567ffb3d1f1d7a898
yashdudhatra <yashdudhatra001@gmail.com>
2021-03-02 10:00:17 +0530

    commit 1

---
+++ app/src/main/java/com/yash1213/miic/UpdatesActivity.java
@@ -0,0 +1,14 @@
+package com.yash1213.miic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdatesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updates);
+    }
+}
